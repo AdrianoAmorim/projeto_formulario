b4b2b352f391b3561fe92177234e141301118a41
Adriano Amorim <adryanoamorim@hotmail.com>
2023-02-09 16:30:41 -0300

    feat: valida os inputs, todos serao requeridos

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,38 +20,57 @@ var objForms = {}
 
 //FUNCAO QUE PEGA OS VALORES DOS INPUTS E SETA NA VARIAVEL (oBJETO)
 
-const nextpage = () =>{
+const nextpage = () => {
   let header2 = document.getElementById('header2');
   let page2 = document.getElementById('page2');
-    header.classList.toggle('ocultar');
-    page.classList.toggle('ocultar');
-    header2.classList.toggle('ocultar')
-    page2.classList.toggle('ocultar')
+  header.classList.toggle('ocultar');
+  page.classList.toggle('ocultar');
+  header2.classList.toggle('ocultar')
+  page2.classList.toggle('ocultar')
 
 }
 
-const setDados = ()=>{
+const setDados = () => {
   objForms.nome = inptNome.value;
-  objForms.sobreNome=inptSobreNome.value
-  objForms.email=inptEmail.value;
-  objForms.senha=inptSenha.value;
-  objForms.mentorNome=inptMentorName.value;
-  objForms.dataMentoria=inptDataMentoria.value;
-  objForms.dataDas=inptDataDas.value;
-  objForms.dataAte=inptDataAte.value;
-  objForms.segundaDataMentoria=inptSegundaDataMentoria.value;
-  objForms.segundaDataDas=inptSegundaDataDas.value;
-  objForms.segundaDataAte=inptSegundaDataAte.value;
+  objForms.sobreNome = inptSobreNome.value
+  objForms.email = inptEmail.value;
+  objForms.senha = inptSenha.value;
+  objForms.mentorNome = inptMentorName.value;
+  objForms.dataMentoria = inptDataMentoria.value;
+  objForms.dataDas = inptDataDas.value;
+  objForms.dataAte = inptDataAte.value;
+  objForms.segundaDataMentoria = inptSegundaDataMentoria.value;
+  objForms.segundaDataDas = inptSegundaDataDas.value;
+  objForms.segundaDataAte = inptSegundaDataAte.value;
 
-  nextpage()
+  nextpage();
+}
+const validarCampos = () => {
+  //PEGA TODOS OS INPUTS
+  let inputs = document.querySelectorAll("input");
+  //USADO PARA CONTROLAR QUANTOS CAMPOS FORAM PREENCHIDOS
+  let countInpt = 0;
+
+  //PERCORRE O ARRAY COM TODOS OS INPUTS
+  //O FOREACH ELE RECEBE COMO PARAMETRO UM ITEM DE CADA VEZ DO ARRAY.. 
+  //A CADA ITERACAO DO FOREACH ELE USA UM ITEM DO ARRAY...
+  inputs.forEach(item => {
+    //TESTA SE O TAMANHO DO QUE TEM NO INPUT É MAIOR  Q 0 (SE ELE ESTA VAZIO)
+    if (item.value.length > 0) {
+      countInpt++;
+    }
+
+  });
+  //AQUI TESTA SE OS 11 CAMPOS ESTAO PREENCHIDOS, SE ESTIVER SET OS DADOS, SE NAO MANDA O ALERT 
+  if(countInpt == 11){
+    setDados();
+  }else{
+    alert("Porfavor, Preencha todos os Campos!- ;)");
+  }
+ 
 }
 
 //CRIANDO OS EVENTOS
 btnSalvar.addEventListener("click", () => {
-    setDados();
-
-    localStorage.setItem("nome",objForms.nome);
-
-    let nome = localStorage.getItem("nome");
-    console.log(objForms);
+  validarCampos();
 })
